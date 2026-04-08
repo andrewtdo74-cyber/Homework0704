a693849d709793d281e6572847823119ee907f1a
andrewtdo74 <andrewtdo74@gmail.com>
2026-04-09 00:27:12 +0700

    12

---
+++ Bailamthem1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Đề Bài: Xây dựng một ứng dụng chuyển đổi tiền tệ, cho phép tính giá trị tiền VNĐ dựa trên một tỉ giá cho trước và giá trị tiền USD được nhập vào.
+long Tygia = 23000;
+Console.WriteLine("Ty Gia: " + Tygia);
+
+Console.Write("Nhap tien USD: ");
+long USD = long.Parse(Console.ReadLine());
+
+long VND = USD * Tygia;
+Console.Write("Tien VND: " + VND);
